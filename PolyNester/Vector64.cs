d941e242c133f0d10ee822ae043030a040033aa1
mostafa Mahmoud <mostafa_nanticock@hotmail.com>
2021-11-20 19:27:29 +0200

    Code separated into different modules

---
+++ PolyNester/Vector64.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolyNester
+{
+    public struct Vector64
+    {
+        public double X;
+        public double Y;
+        public Vector64(double X, double Y)
+        {
+            this.X = X; this.Y = Y;
+        }
+
+        public static Vector64 operator +(Vector64 a, Vector64 b)
+        {
+            return new Vector64(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Vector64 operator *(Vector64 a, Vector64 b)
+        {
+            return new Vector64(a.X * b.X, a.Y * b.Y);
+        }
+
+        public static Vector64 operator *(double b, Vector64 a)
+        {
+            return new Vector64(a.X * b, a.Y * b);
+        }
+    }
+}
